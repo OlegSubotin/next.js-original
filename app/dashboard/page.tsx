958215d3bfd653f3c8d6f78d2f4438b63a6d6c29
Oleg <87891790+OlegSubotin@users.noreply.github.com>
2023-11-29 15:55:02 +0200

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <div>Dashboard PAge</div>;
+  return (
+    <>
+      <div>Dashboard PAge</div>
+      <div>Dashboard PAge</div>
+      <div>Dashboard PAge</div>
+    </>
+  );
 }
